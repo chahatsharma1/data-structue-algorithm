d7b7a0668373c7660c42e2e3cacd8a189f760777
chahats2002@gmail.com <chahats2002@gmail.com>
2024-10-12 01:28:41 +0530

    tree questions

---
+++ Tree/src/LeftView.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LeftView {
+    public ArrayList<Integer> rightSideView(TreeNode root) {
+        ArrayList<Integer> result = new ArrayList<>();
+
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode current = queue.poll();
+                if (i == 0){
+                    result.add(current.val);
+                }
+                if (current.left != null) {
+                    queue.offer(current.left);
+                }
+                if (current.right != null) {
+                    queue.offer(current.right);
+                }
+            }
+        }
+        return result;
+    }
+}
