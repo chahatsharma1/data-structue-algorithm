3a43321c9f7e8fbc4ed0268cf91eaa27afa6c310
chahats2002@gmail.com <chahats2002@gmail.com>
2024-10-15 02:15:28 +0530

    new tree questions

--- Tree/src/BSTFromPreOrder.java
+++ Tree/src/BSTFromPreOrder.java
@@ -18,5 +18,4 @@ public class BSTFromPreOrder {
 
         return node;
     }
-
 }
---
+++ Tree/src/Merge.java
@@ -0,0 +1,17 @@
+public class Merge {
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 == null){
+            return root2;
+        }
+        if (root2 == null){
+            return root1;
+        }
+
+        TreeNode newNode = new TreeNode(root1.data + root2.data);
+
+        newNode.left = mergeTrees(root1.left, root2.left);
+        newNode.right = mergeTrees(root1.right, root2.right);
+
+        return newNode;
+    }
+}
