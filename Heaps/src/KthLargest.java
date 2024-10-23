aaf63063c065c64d0b16874cedb504fcccfb91ae
chahats2002@gmail.com <chahats2002@gmail.com>
2024-10-24 01:26:15 +0530

    new questions

---
+++ Heaps/src/KthLargest.java
@@ -0,0 +1,25 @@
+import java.util.PriorityQueue;
+
+public class KthLargest {
+    int k;
+    PriorityQueue<Integer> minHeap;
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        minHeap = new PriorityQueue<>(k);
+
+        for (int num : nums){
+            add(num);
+        }
+    }
+    
+    public int add(int val) {
+        if (minHeap.size() < k){
+            minHeap.add(val);
+        } else if (minHeap.peek() < val) {
+            minHeap.poll();
+            minHeap.add(val);
+        }
+        return minHeap.peek();
+    }
+}
