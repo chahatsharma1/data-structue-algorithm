f39f9fc1bbf1bedcf772104128c051ddd678766f
chahats2002@gmail.com <chahats2002@gmail.com>
2024-10-28 01:40:29 +0530

    new questions

---
+++ LinkedList/src/RemoveDuplicates.java
@@ -0,0 +1,16 @@
+public class RemoveDuplicates {
+    public ListNode deleteDuplicates(ListNode node) {
+        if (node == null){
+            return node;
+        }
+        ListNode head = node;
+        while (node.next != null){
+            if (node.val == node.next.val){
+                node.next = node.next.next;
+            } else {
+                node = node.next;
+            }
+        }
+        return head;
+    }
+}
---
+++ LinkedList/src/RemoveDuplicates2.java
@@ -0,0 +1,23 @@
+public class RemoveDuplicates2 {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null){
+            return head;
+        }
+
+        ListNode dummy = new ListNode(0, head);
+        ListNode prev = dummy;
+
+        while (head != null){
+            if (head.next != null && head.val == head.next.val){
+                while (head.next != null && head.val == head.next.val){
+                    head = head.next;
+                }
+                prev.next = head.next;
+            } else {
+                prev = prev.next;
+            }
+            head = head.next;
+        }
+        return dummy.next;
+    }
+}
