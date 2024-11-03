157fd50ea8058bb8169cb44c5719f8638b8976b2
chahats2002@gmail.com <chahats2002@gmail.com>
2024-11-04 00:06:54 +0530

    new questions

---
+++ StackAndQueue/src/BasicCalculator.java
@@ -0,0 +1,40 @@
+import java.util.Stack;
+
+public class BasicCalculator {
+    public int calculate(String s){
+        Stack<Integer> stack = new Stack<>();
+        int currentNumber = 0;
+        int ans = 0;
+        int sign = 1;
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (Character.isDigit(ch)){
+                currentNumber = currentNumber * 10 + (ch - '0');
+            } else if (ch == '+') {
+                ans += sign * currentNumber;
+                currentNumber = 0;
+                sign = 1;
+            } else if (ch == '-'){
+                ans += sign * currentNumber;
+                currentNumber = 0;
+                sign = -1;
+            } else if (ch == '(') {
+                stack.push(ans);
+                stack.push(sign);
+                ans = 0;
+                sign = 1;
+            } else if (ch == ')'){
+                ans += sign * currentNumber;
+                ans *= stack.pop();
+                ans += stack.pop();
+                currentNumber = 0;
+            }
+        }
+        if (currentNumber != 0){
+            ans += currentNumber * sign;
+        }
+        return ans;
+    }
+}
