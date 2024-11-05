fdb2fe7eb6449707779d01754dcaa016c5d126d9
chahats2002@gmail.com <chahats2002@gmail.com>
2024-11-05 23:31:59 +0530

    new questions

---
+++ Strings/src/BeautifulString.java
@@ -0,0 +1,12 @@
+public class BeautifulString {
+    public int minChanges(String s) {
+        int ans = 0;
+
+        for (int i = 0; i + 1 < s.length(); i += 2) {
+            if (s.charAt(i) != s.charAt(i + 1)){
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
