ab13a88f059000bae2ff65e8c175aa7ef09513f4
chahats2002@gmail.com <chahats2002@gmail.com>
2024-11-09 01:44:19 +0530

    new questions

---
+++ Array/src/GasStation.java
@@ -0,0 +1,19 @@
+public class GasStation {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int totalTank = 0;
+        int currentTank = 0;
+        int start = 0;
+
+        for (int i = 0; i < gas.length; i++){
+            int net = gas[i] - cost[i];
+            totalTank += net;
+            currentTank += net;
+
+            if (currentTank < 0){
+                currentTank = 0;
+                start = i + 1;
+            }
+        }
+        return totalTank >= 0 ? start : -1;
+    }
+}
