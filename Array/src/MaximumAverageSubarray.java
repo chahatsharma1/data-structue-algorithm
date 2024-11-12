8049487f50521dc18e11b0af0e395b309ab7a772
chahats2002@gmail.com <chahats2002@gmail.com>
2024-11-12 23:08:13 +0530

    new questions

---
+++ Array/src/MaximumAverageSubarray.java
@@ -0,0 +1,17 @@
+public class MaximumAverageSubarray {
+    public double findMaxAverage(int[] nums, int k) {
+        double maxSum = 0;
+
+        for (int i = 0; i < k; i++) {
+            maxSum += nums[i];
+        }
+
+        double currentSum = maxSum;
+
+        for (int i = k; i < nums.length; i++) {
+            currentSum += nums[i] - nums[i - k];
+            maxSum = Math.max(currentSum, maxSum);
+        }
+        return maxSum / k;
+    }
+}
