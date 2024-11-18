d455a5adee8d8eee22ad1b04e82393430f78d269
chahats2002@gmail.com <chahats2002@gmail.com>
2024-11-19 00:06:38 +0530

    new questions

---
+++ StackAndQueue/src/RemovingStars.java
@@ -0,0 +1,20 @@
+import java.util.Stack;
+
+public class RemovingStars {
+    public String removeStars(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()){
+            if (ch == '*'){
+                stack.pop();
+            } else {
+                stack.push(ch);
+            }
+        }
+        StringBuilder ans = new StringBuilder();
+        for (char ch : stack){
+            ans.append(ch);
+        }
+        return ans.toString();
+    }
+}
