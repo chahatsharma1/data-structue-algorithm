7135b3891ab12e2b507135735f1da6756cf1c4b7
chahats2002@gmail.com <chahats2002@gmail.com>
2024-12-10 23:58:01 +0530

    new questions

--- LinkedList/src/InsertionSortList.java
+++ LinkedList/src/InsertionSortList.java
@@ -1,5 +1,24 @@
 public class InsertionSortList {
     public ListNode insertionSortList(ListNode head) {
+        if (head == null){
+            return null;
+        }
 
+        ListNode dummy = new ListNode(0);
+        ListNode current = head;
+
+        while (current != null){
+            ListNode prev = dummy;
+            ListNode next = current.next;
+
+            while (prev.next != null && prev.next.val < current.val){
+                prev = prev.next;
+            }
+
+            current.next = prev.next;
+            prev.next = current;
+            current = next;
+        }
+        return dummy.next;
     }
 }
