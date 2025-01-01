f2e0ecc78b052a655386f96261fd3eea41db340e
chahats2002@gmail.com <chahats2002@gmail.com>
2025-01-01 18:54:00 +0530

    new questions

---
+++ Strings/src/MaximumScoreSplit.java
@@ -0,0 +1,24 @@
+public class MaximumScoreSplit {
+    public int maxScore(String s) {
+        int totalOnes = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '1') {
+                totalOnes++;
+            }
+        }
+
+        int maxScore = Integer.MIN_VALUE;
+        int leftZeros = 0, rightOnes = totalOnes;
+
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) == '0') {
+                leftZeros++;
+            } else {
+                rightOnes--;
+            }
+            int score = leftZeros + rightOnes;
+            maxScore = Math.max(maxScore, score);
+        }
+        return maxScore;
+    }
+}
