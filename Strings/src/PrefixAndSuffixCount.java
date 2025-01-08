7dc8824113bcaa08c484b5ca54448d58d4fd1b34
chahats2002@gmail.com <chahats2002@gmail.com>
2025-01-08 22:16:36 +0530

    new questions

---
+++ Strings/src/PrefixAndSuffixCount.java
@@ -0,0 +1,19 @@
+public class PrefixAndSuffixCount {
+    public static int countPrefixAndSuffixPairs(String[] words) {
+        int count = 0;
+        int n = words.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (isPrefixAndSuffix(words[i], words[j])) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    private static boolean isPrefixAndSuffix(String str1, String str2) {
+        return str2.startsWith(str1) && str2.endsWith(str1);
+    }
+}
