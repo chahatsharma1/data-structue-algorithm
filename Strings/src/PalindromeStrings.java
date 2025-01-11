1fb8d6690163d9581359702b6cc97179e475d532
chahats2002@gmail.com <chahats2002@gmail.com>
2025-01-11 21:23:13 +0530

    new questions

---
+++ Strings/src/PalindromeStrings.java
@@ -0,0 +1,20 @@
+public class PalindromeStrings {
+    public static boolean canConstruct(String s, int k) {
+        if (k > s.length()) {
+            return false;
+        }
+
+        int[] freq = new int[26];
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
+        int oddCount = 0;
+        for (int count : freq) {
+            if (count % 2 != 0) {
+                oddCount++;
+            }
+        }
+        return k >= oddCount;
+    }
+}
