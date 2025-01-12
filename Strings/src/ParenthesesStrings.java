8b341f26bf2f521430f76bf767ab4744372408d3
chahats2002@gmail.com <chahats2002@gmail.com>
2025-01-12 20:14:24 +0530

    new questions

---
+++ Strings/src/ParenthesesStrings.java
@@ -0,0 +1,57 @@
+public class ParenthesesStrings {
+    public static boolean canBeValid(String s, String locked) {
+        int n = s.length();
+        if (n % 2 != 0){
+            return false;
+        }
+
+        int balance = 0;
+        int flexible = 0;
+        for (int i = 0; i < n; i++){
+            if (locked.charAt(i) == '0'){
+                flexible++;
+            } else if (s.charAt(i) == '('){
+                balance++;
+            } else {
+                balance--;
+            }
+
+            if (balance < 0){
+                if (flexible > 0){
+                    balance++;
+                    flexible--;
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        balance = 0;
+        flexible = 0;
+        for (int i = n - 1; i >= 0; i--){
+            if (locked.charAt(i) == '0'){
+                flexible++;
+            } else if (s.charAt(i) == ')'){
+                balance++;
+            } else {
+                balance--;
+            }
+
+            if (balance < 0){
+                if (flexible > 0){
+                    balance++;
+                    flexible--;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s = "()";
+        String locked = "11";
+        System.out.println(canBeValid(s, locked));
+    }
+}
