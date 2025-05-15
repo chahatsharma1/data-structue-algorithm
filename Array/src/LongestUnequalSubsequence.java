2ce4be4a882625b1ce6b45bcf839b883fcb9d784
chahats2002@gmail.com <chahats2002@gmail.com>
2025-05-16 00:03:26 +0530

    new questions

--- Array/src/LongestUnequalSubsequence.java
+++ Array/src/LongestUnequalSubsequence.java
@@ -1,7 +1,18 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class LongestUnequalSubsequence {
     public List<String> getLongestSubsequence(String[] words, int[] groups) {
-        
+        List<String> result = new ArrayList<>();
+        int lastGroup = -1;
+
+        for (int i = 0; i < words.length; i++) {
+            if (lastGroup == -1 || groups[i] != lastGroup) {
+                result.add(words[i]);
+                lastGroup = groups[i];
+            }
+        }
+
+        return result;
     }
 }
