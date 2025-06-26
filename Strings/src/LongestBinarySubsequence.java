d6063c843de6189974eae4b1ded3ab916122e48d
chahats2002@gmail.com <chahats2002@gmail.com>
2025-06-26 11:59:50 +0530

    new questions

---
+++ Strings/src/LongestBinarySubsequence.java
@@ -0,0 +1,18 @@
+public class LongestBinarySubsequence {
+    public int longestSubsequence(String s, int k) {
+        int oneCount = 0;
+        int num = 0;
+        int pow = 1;
+
+        for (int i = s.length() - 1; i >= 0 && num + pow <= k; --i) {
+            if (s.charAt(i) == '1') {
+                ++oneCount;
+                num += pow;
+            }
+            pow *= 2;
+        }
+
+        long zeroCount = s.chars().filter(c -> c == '0').count();
+        return (int) zeroCount + oneCount;
+    }
+}
