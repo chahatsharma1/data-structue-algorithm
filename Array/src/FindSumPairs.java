43a557b7b45d2fb82b75091b7e9969693ddaa8b4
chahats2002@gmail.com <chahats2002@gmail.com>
2025-07-06 19:24:39 +0530

    new questions

---
+++ Array/src/FindSumPairs.java
@@ -0,0 +1,14 @@
+public class FindSumPairs {
+
+    public FindSumPairs(int[] nums1, int[] nums2) {
+        
+    }
+    
+    public void add(int index, int val) {
+        
+    }
+    
+    public int count(int tot) {
+        
+    }
+}
