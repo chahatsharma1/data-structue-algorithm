55d7ede6f81aa00ae6cc3edad9a80cc58f003ad4
chahats2002@gmail.com <chahats2002@gmail.com>
2025-07-07 04:18:22 +0530

    new questions

--- Array/src/FindSumPairs.java
+++ Array/src/FindSumPairs.java
@@ -1,14 +1,39 @@
+import java.util.*;
+
 public class FindSumPairs {
+    int[] nums1;
+    int[] nums2;
+    Map<Integer, Integer> freqMap;
 
     public FindSumPairs(int[] nums1, int[] nums2) {
-        
+        this.nums1 = nums1;
+        this.nums2 = nums2;
+        this.freqMap = new HashMap<>();
+
+        for (int num : nums2) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
     }
-    
+
     public void add(int index, int val) {
-        
+        int oldVal = nums2[index];
+        int newVal = oldVal + val;
+
+        freqMap.put(oldVal, freqMap.get(oldVal) - 1);
+        if (freqMap.get(oldVal) == 0) {
+            freqMap.remove(oldVal);
+        }
+
+        nums2[index] = newVal;
+        freqMap.put(newVal, freqMap.getOrDefault(newVal, 0) + 1);
     }
-    
+
     public int count(int tot) {
-        
+        int result = 0;
+        for (int num : nums1) {
+            int complement = tot - num;
+            result += freqMap.getOrDefault(complement, 0);
+        }
+        return result;
     }
 }
