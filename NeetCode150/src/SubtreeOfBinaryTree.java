0cca2b5e7140abcd969d6d829be85c55891e7416
chahats2002@gmail.com <chahats2002@gmail.com>
2025-07-21 11:37:47 +0530

    new questions

--- NeetCode150/src/SubtreeOfBinaryTree.java
+++ NeetCode150/src/SubtreeOfBinaryTree.java
@@ -1,11 +1,22 @@
 public class SubtreeOfBinaryTree {
-    public boolean isSameTree(TreeNode node1, TreeNode node2) {
-        if (node1 == null && node2 == null){
-            return true;
-        }
-        if (node1 == null || node2 == null || node1.val != node2.val){
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (root == null){
             return false;
         }
-        return isSameTree(node1.left, node2.left) && isSameTree(node1.right, node2.right);
+        return isSame(root, subRoot) || isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
+    private boolean isSame(TreeNode node1, TreeNode node2){
+        if(node1 == null && node2 == null){
+            return true;
+        }
+        if(node1 == null || node2 == null){
+            return false;
+        }
+
+        if(node1.val != node2.val){
+            return false;
+        }
+        return isSame(node1.left, node2.left) && isSame(node1.right, node2.right);
     }
 }
