6dca4c5c113669848f41e352cd3363e3b4d6c98a
chahats2002@gmail.com <chahats2002@gmail.com>
2025-09-12 12:26:50 +0530

    new questions

---
+++ Strings/src/VowelSubstringGame.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class VowelSubstringGame {
+    public boolean doesAliceWin(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            switch (c) {
+                case 'a', 'e', 'i', 'o', 'u':
+                    return true;
+            }
+        }
+
+        return false;
+    }
+}
