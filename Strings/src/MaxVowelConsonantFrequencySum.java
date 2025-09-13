5522aabcdb368147d20c78cd9f8c26bd87e7c2e0
chahats2002@gmail.com <chahats2002@gmail.com>
2025-09-13 14:31:06 +0530

    new questions

---
+++ Strings/src/MaxVowelConsonantFrequencySum.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class MaxVowelConsonantFrequencySum {
+    public static int maxVowelConsonantSum(String s) {
+        int[] freq = new int[26];
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
+        Set<Character> vowels = new HashSet<>(Arrays.asList('a','e','i','o','u'));
+
+        int maxVowel = 0;
+        int maxConsonant = 0;
+
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] > 0) {
+                char ch = (char)(i + 'a');
+                if (vowels.contains(ch)) {
+                    maxVowel = Math.max(maxVowel, freq[i]);
+                } else {
+                    maxConsonant = Math.max(maxConsonant, freq[i]);
+                }
+            }
+        }
+
+        return maxVowel + maxConsonant;
+    }
+}
