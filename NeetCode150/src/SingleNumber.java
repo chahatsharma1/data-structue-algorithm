b75e45ceecb91aad1e87cd0a3560cd39b5fcc073
chahats2002@gmail.com <chahats2002@gmail.com>
2025-09-21 12:51:08 +0530

    new questions

---
+++ NeetCode150/src/SingleNumber.java
@@ -0,0 +1,9 @@
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        int unique = 0;
+        for(int n : nums) {
+            unique ^= n;
+        }
+        return unique;
+    }
+}
