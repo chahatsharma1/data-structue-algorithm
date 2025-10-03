de556a95085d90d8044678b3b1eba8094d62f0be
chahats2002@gmail.com <chahats2002@gmail.com>
2025-10-03 22:51:16 +0530

    new questions

---
+++ Graph/src/Cell.java
@@ -0,0 +1,11 @@
+public class Cell {
+    int row;
+    int col;
+    int height;
+
+    public Cell(int row, int col, int height) {
+        this.row = row;
+        this.col = col;
+        this.height = height;
+    }
+}
