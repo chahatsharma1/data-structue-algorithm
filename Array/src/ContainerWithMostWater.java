9b61d83904ece6066781ed1079680f71b1fdc12a
chahats2002@gmail.com <chahats2002@gmail.com>
2025-10-04 11:35:21 +0530

    new questions

---
+++ Array/src/ContainerWithMostWater.java
@@ -0,0 +1,18 @@
+public class ContainerWithMostWater {
+    public int maxArea(int[] height) {
+        int max = 0;
+        int i = 0;
+        int j = height.length - 1;
+
+        while (i < j){
+            if (height[i] < height[j]){
+                max = Math.max(max, height[i] * (j - i));
+                i++;
+            } else {
+                max = Math.max(max, height[j] * (j - i));
+                j--;
+            }
+        }
+        return max;
+    }
+}
