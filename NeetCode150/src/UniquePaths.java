ee9c34b08c3a166cfc03a119244e4f182dbbb069
chahats2002@gmail.com <chahats2002@gmail.com>
2025-10-09 16:57:10 +0530

    new questions

---
+++ NeetCode150/src/UniquePaths.java
@@ -0,0 +1,19 @@
+public class UniquePaths {
+    public int uniquePaths(int m, int n) {
+        int[] prev = new int[n];
+        for (int i = 0; i < m; i++) {
+            int[] temp = new int[n];
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    temp[j] = 1;
+                } else if (j == 0) {
+                    temp[j] = prev[j];
+                } else {
+                    temp[j] = prev[j] + temp[j - 1];
+                }
+            }
+            prev = temp;
+        }
+        return prev[n - 1];
+    }
+}
