b5cbdd5462f5a0171dbcf082facd3da63c808985
chahats2002@gmail.com <chahats2002@gmail.com>
2025-11-06 23:57:56 +0530

    new questions

---
+++ Graph/src/Station.java
@@ -0,0 +1,11 @@
+public class Station {
+    public int id;
+    public boolean isOffline = false;
+    public int gridId = -1;
+
+    public Station() {}
+
+    public Station(int id) {
+        this.id = id;
+    }
+}
