c62e1e569276e7d489ead6f07b8499ba9a674e60
chahats2002@gmail.com <chahats2002@gmail.com>
2025-11-08 14:25:51 +0530

    new questions

---
+++ Bit/src/MinimumBitOperationsToZero.java
@@ -0,0 +1,13 @@
+public class MinimumBitOperationsToZero {
+    public int minimumOneBitOperations(int n) {
+        if (n == 0){
+            return 0;
+        }
+        int x = 1;
+        while (x * 2 <= n) {
+            x <<= 1;
+        }
+
+        return minimumOneBitOperations(n ^ (x | x >> 1)) + 1 + x - 1;
+    }
+}
