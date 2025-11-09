ab4a3513c21d248d13f877a8447c4b7db183a07d
chahats2002@gmail.com <chahats2002@gmail.com>
2025-11-09 10:05:42 +0530

    new questions

---
+++ Array/src/CountOperationsToObtainZero.java
@@ -0,0 +1,16 @@
+public class CountOperationsToObtainZero {
+    public int countOperations(int num1, int num2) {
+        int count = 0;
+
+        while(num1 != 0 && num2 != 0){
+            if (num1 >= num2){
+                num1 = num2 - num1;
+
+            } else {
+                num2 = num1 - num2;
+            }
+            count++;
+        }
+        return count;
+    }
+}
