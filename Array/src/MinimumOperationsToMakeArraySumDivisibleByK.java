fe217c0cd7ee91c9b530b83aa5518c056de01fa7
chahats2002@gmail.com <chahats2002@gmail.com>
2025-11-29 18:51:01 +0530

    new questions

---
+++ Array/src/MinimumOperationsToMakeArraySumDivisibleByK.java
@@ -0,0 +1,9 @@
+public class MinimumOperationsToMakeArraySumDivisibleByK {
+    public int minOperations(int[] nums, int k) {
+        int sum = 0;
+        for (int num : nums){
+            sum += num;
+        }
+        return sum % k;
+    }
+}
