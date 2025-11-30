7d2dc81328cdade09be8492afd5f7bf2606ef100
chahats2002@gmail.com <chahats2002@gmail.com>
2025-11-30 14:17:01 +0530

    new questions

---
+++ Array/src/MakeSumDivisibleByP.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class MakeSumDivisibleByP {
+    public int minSubarray(int[] nums, int p) {
+        long total = 0;
+        for (int x : nums) {
+            total += x;
+        }
+        int target = (int) (total % p);
+
+        if (target == 0) {
+            return 0;
+        }
+
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+
+        long prefix = 0;
+        int result = nums.length;
+
+        for (int i = 0; i < nums.length; i++) {
+            prefix = (prefix + nums[i]) % p;
+            int need = (int) ((prefix - target + p) % p);
+
+            if (map.containsKey(need)) {
+                result = Math.min(result, i - map.get(need));
+            }
+
+            map.put((int) prefix, i);
+        }
+        return result == nums.length ? -1 : result;
+    }
+}
