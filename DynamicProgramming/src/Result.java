3d3563c05376638b34c6bd3c53837685b4aecd37
chahats2002@gmail.com <chahats2002@gmail.com>
2025-12-16 12:09:54 +0530

    new questions

---
+++ DynamicProgramming/src/Result.java
@@ -0,0 +1,11 @@
+public class Result {
+    int[] dp0;
+    int[] dp1;
+    int size;
+
+    Result(int[] dp0, int[] dp1, int size) {
+        this.dp0 = dp0;
+        this.dp1 = dp1;
+        this.size = size;
+    }
+}
