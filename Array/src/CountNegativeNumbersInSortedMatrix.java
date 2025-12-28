c8e40f444be5c9d543a771c3019dbc5c4d1d5824
chahats2002@gmail.com <chahats2002@gmail.com>
2025-12-28 13:35:13 +0530

    new questions

---
+++ Array/src/CountNegativeNumbersInSortedMatrix.java
@@ -0,0 +1,13 @@
+public class CountNegativeNumbersInSortedMatrix {
+    public int countNegatives(int[][] grid) {
+        int count = 0;
+        for (int[] ints : grid) {
+            for (int anInt : ints) {
+                if (anInt < 0) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
